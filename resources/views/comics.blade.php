e553bdbac2923df651cca2a3d175b61eada3e9cf
andrearomeo04 <pisco004@gmail.com>
2024-10-03 21:38:42 +0200

    [UPD] copied views and layouts from laravel comic

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<!-- jumbo section -->
+    <div class="jumbo">
+        <div class="container">
+            <div class="row"></div>
+        </div>
+    </div>
+
+    <!-- comics section -->
+     <div class="bg-black py-5">
+         <div class="container">
+             <div class="row">
+     
+             </div>
+         </div>
+     </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+<div class="container">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-between align-items-center py-3">
+            <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+            <ul class="list-unstyled d-flex mb-0">
+                <li class="mx-3">CHARACTERS</li>
+                <li class="mx-3">COMICS</li>
+                <li class="mx-3">MOVIES</li>
+                <li class="mx-3">TV</li>
+                <li class="mx-3">GAMES</li>
+                <li class="mx-3">COLLECTIBLES</li>
+                <li class="mx-3">VIDEOS</li>
+                <li class="mx-3">FANS</li>
+                <li class="mx-3">NEWS</li>
+                <li class="mx-3">SHOP</li>
+            </ul>
+            <div class="search">
+                <input type="search" placeholder="cerca">
+            </div>
+        </div>
+    </div>
+</div>
+</header>
