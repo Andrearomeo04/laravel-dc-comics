adb7d3c38ea31144794ce6b4cb3a910fec585005
andrearomeo04 <pisco004@gmail.com>
2024-10-03 23:56:52 +0200

    [UPD] created show

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,20 @@
+@extendes('layout.app')
+
+@section('content')
+<div class="bg-black py-5">
+         <div class="container">
+             <div class="row">
+                <div class="col-4">
+                        <div class="thumbnail d-flex justify-content-start">
+                            <img src="{{ $comic['thumb'] }}" alt="" class="img-fluid">
+                        </div>
+                        <h2 class="text-center">{{ $comic['title'] }}</h2>
+                        <p>{{ $comic['description'] }}</p>
+                        <p>{{ $comic['type'] }}</p>
+                        <h5>{{ $comic['price'] }}</h5>
+                        <p>{{ $comic['sale_date'] }}</p>
+                </div>  
+             </div>
+         </div>
+     </div>
+@endsection
