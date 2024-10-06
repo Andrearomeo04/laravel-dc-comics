0b0a371faa965011c2eedde2c91b91023f672c9c
andrearomeo04 <pisco004@gmail.com>
2024-10-06 23:43:36 +0200

    [UPD] created create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <form action="{{ route('comics.store') }}" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-12">
+                        <label for="" class="control-label"></label>
+                        <input type="text" name="title" class="form-control" placeholder="title">
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="control-label"></label>
+                        <input type="text" name="title" class="form-control" placeholder="description">
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="control-label"></label>
+                        <input type="text" name="title" class="form-control" placeholder="type">
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="control-label"></label>
+                        <input type="text" name="title" class="form-control" placeholder="price">
+                    </div>
+                    <div class="col-12">
+                        <label for="" class="control-label"></label>
+                        <input type="text" name="title" class="form-control" placeholder="sale date">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
